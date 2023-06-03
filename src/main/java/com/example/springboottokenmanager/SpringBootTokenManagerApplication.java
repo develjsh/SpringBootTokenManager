b96f666ffc73d4622705d7f0b6e3a4c30433fc4b
Jung Seung Hwan <develjsh@gmail.com>
2023-06-03 13:29:40 +0900

    first commit

---
+++ src/main/java/com/example/springboottokenmanager/SpringBootTokenManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottokenmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTokenManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTokenManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboottokenmanager/SpringBootTokenManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottokenmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTokenManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
